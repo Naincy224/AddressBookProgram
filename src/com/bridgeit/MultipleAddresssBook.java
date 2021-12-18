96b0713ff12a38e269ed0dce19041a8ce75e399f
Naincy224 <pihusingh224@gmail.com>
2021-12-18 08:55:36 +0530

    Added: search contact in multiple address book

--- src/com/bridgeit/MultipleAddresssBook.java
+++ src/com/bridgeit/MultipleAddresssBook.java
@@ -1,6 +1,7 @@
 package com.bridgeit;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class MultipleAddresssBook {
@@ -13,7 +14,7 @@ public class MultipleAddresssBook {
 		ContactDetail contactDetail = ContactDetail.getInstance();
 			do {
 				Scanner sc = new Scanner(System.in);
-				System.out.println("What do you want to do:\n1. Create AddressBook\n2. View AddressBook\n3. Exit");
+				System.out.println("What do you want to do:\n1. Create AddressBook\n2. View AddressBook\n3. Search AddressBook\n4. Exit");
 				input = sc.nextInt();
 				switch (input) {
 				case 1: {
@@ -36,8 +37,37 @@ public class MultipleAddresssBook {
 					}
 					break;
 				}
+				case 3: {
+					if (contactDetail.addressBookMap.isEmpty() == true) {
+						System.out.println("Address book is empty");
+					} else {
+						System.out.println(contactDetail.addressBookMap.keySet());
+						Scanner sc2 = new Scanner(System.in);
+						Scanner sc3 = new Scanner(System.in);
+						System.out.println("Enter the Address Book Name you want to search");
+						String addressBookName = sc2.nextLine();
+						if (contactDetail.addressBookMap.get(addressBookName) == null) {
+							System.out.println("Address Book Not found with this name");
+						} else {
+							if (contactDetail.addressBookMap.get(addressBookName).size() > 0) {
+								List<Contact> list = contactDetail.addressBookMap.get(addressBookName);
+								System.out.println("Enter the city you want to search");
+								String city = sc3.nextLine();
+								for (int i = 0; i < list.size(); i++) {
+									String fname = list.get(i).city;
+									if (fname.equals(city)) {
+										System.out.println(list.get(i));
+									}
+								}
+							} else {
+								System.out.println("Address Book is empty");
+							}
+						}
+					}
+					break;
 				}
-		}while(input != 3);
-	}
+				}
+			} while (input != 4);
+		}
 
 }
